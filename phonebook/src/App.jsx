887b1f98e31d598174a21a41148bc81cf918f231
Sean <seanhoyeonjin@gmail.com>
2024-01-23 13:05:36 -0500

    finished exercise 2.8

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState(undefined);
 
-  // handles change of input
+  // handles change of name input
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
+  };
+
+  // handles change of number input
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   // handles input submission
@@ -16,16 +22,23 @@ const App = () => {
     event.preventDefault();
     const nameObject = {
       name: newName,
+      number: newNumber,
     };
 
-    // find out if the name entered already exists in the phonebook
-    const duplicate = persons.find((person) => person.name === newName);
-    if (duplicate) {
-      alert(`${newName} already exists in the phonebook`);
+    // find out if the name or number entered already exists in the phonebook
+    const duplicateName = persons.find((person) => person.name === newName);
+    const duplicateNumber = persons.find(
+      (person) => person.number === newNumber
+    );
+
+    if (duplicateName || duplicateNumber) {
+      alert(`${newName} or ${newNumber} already exists in the phonebook`);
       setNewName("");
+      setNewNumber("");
     } else {
       setPersons(persons.concat(nameObject));
       setNewName("");
+      setNewNumber("");
     }
   };
 
@@ -35,6 +48,9 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
