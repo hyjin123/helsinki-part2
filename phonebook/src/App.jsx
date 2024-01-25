094942bfb47a29da2fe1a7c7cc15cd055fe66369
Sean <seanhoyeonjin@gmail.com>
2024-01-24 20:09:21 -0500

    finished exercise 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Person from "./components/Person";
 import Search from "./components/Search";
 import Form from "./components/Form";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
+
+  // fetch data from the json server
+  useEffect(() => {
+    axios.get("http://localhost:3002/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   // handles change of name input
   const handleNameChange = (event) => {
